cbef6b744d580c288e6ce2b315a88e411bbdd195
이영선 <leedrkr323@konkuk.ac.kr>
2023-07-21 13:08:36 +0900

    Add Greedy - BOJ1339

--- Greedy/BOJ_1339.py
+++ Greedy/BOJ_1339.py
@@ -1,5 +1,21 @@
 # 그리디 알고리즘 - 단어 수학 - 1339번
-## 0~9로 치환해주기
-## 길이가 긴 문자열부터 최댓값을 갖도록 치환, 같은 자릿수에서는 어떻게 나눠주든 상관X
 import sys
 N = int(sys.stdin.readline())
+inputs = []     # 입력 문자열들의 집합
+dict = {}     # 이미 문자->숫자로 변환 완료한 문자들 집합
+sum = 0
+for i in range(N):
+    inputs.append(list(map(str, sys.stdin.readline().strip())))
+for i in range(N):
+    for j in range(len(inputs[i])):
+        tmp = str(inputs[i][j])
+        if tmp in dict:
+            dict[tmp] = dict[tmp]+10**(len(inputs[i])-j-1)
+        else:
+            dict[tmp] = 10**(len(inputs[i])-j-1)
+arr = sorted(dict.items(), key=lambda x:x[1], reverse=True)
+num = 9
+for i in range(len(arr)):
+    sum += arr[i][1]*num
+    num -= 1
+print(sum)
