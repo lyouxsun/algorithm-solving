496ebbe14a3a9f3ae29c78f6204c4a164828a0b8
이영선 <leedrkr323@konkuk.ac.kr>
2023-07-27 12:19:40 +0900

    Add Greedy - BOJ11000

--- Greedy/BOJ_11000.py
+++ Greedy/BOJ_11000.py
@@ -1,21 +1,19 @@
 # 그리디 알고리즘 - 11000번 - 강의실 배정
 ## 일찍 끝나는 수업 먼저 pop -> 그전에 끝난 수업<=시작시간 인 수업 찾아서 push
 import sys, heapq
-N = int(sys.stdin.readline())
+input = sys.stdin.readline
+N = int(input())
 arr = []
 for _ in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-arr.sort()
-end = 0
-i = 0
-cnt = 0
-while arr:
-    if arr[i][0]>=end:
-        end = arr[0][1]
-        del arr[0]
-        i+=1
+    heapq.heappush(arr, list(map(int, input().split())))
+end = [0]
+cnt = 1
+for i in range(N):
+    if arr[0][0] >= end[0]:
+        heapq.heappop(end)
     else:
         cnt += 1
-        i+=1
-
+    heapq.heappush(end, arr[0][1])
+    heapq.heappop(arr)
+print(cnt)
 
