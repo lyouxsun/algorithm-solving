4aa753a627f883301200c080a9f6f98a11192ec2
이영선 <leedrkr323@konkuk.ac.kr>
2023-08-11 15:07:29 +0900

    Add Greedy - BOJ13164

--- BOJ/Greedy/BOJ_13164.py
+++ BOJ/Greedy/BOJ_13164.py
@@ -2,4 +2,12 @@
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
-arr = list(map(int, input.split()))
+arr = list(map(int, input().split()))
+dif = []
+for i in range(N-1):
+    dif.append(arr[i+1]-arr[i])
+dif.sort()
+total = 0
+for i in range(N-K):
+    total += dif[i]
+print(total)
