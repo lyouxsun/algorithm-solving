d180149784a1d563f21f6326659cfa041c9a36cb
이영선 <leedrkr323@konkuk.ac.kr>
2023-08-31 12:00:13 +0900

    Add DynamicProgramming - BOJ11727

---
+++ BOJ/dynamicProgramming/BOJ_11727.py
@@ -0,0 +1,10 @@
+# 다이나믹 프로그래밍 - 11727번 - 2*n 타일링 2
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = [0 for i in range(N)]
+arr[0] = 1
+arr[1] = 3
+for i in range(2, N):
+    arr[i] = arr[i-2]*2+arr[i-1]
+print(arr[N-1]%10007)
