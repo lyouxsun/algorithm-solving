8c0b8cb728847fb4a1e04607514debfa5d235dad
이영선 <leedrkr323@konkuk.ac.kr>
2023-08-31 12:07:54 +0900

    Fix DynamicProgramming - BOJ11727

--- BOJ/dynamicProgramming/BOJ_11727.py
+++ BOJ/dynamicProgramming/BOJ_11727.py
@@ -5,6 +5,10 @@ N = int(input())
 arr = [0 for i in range(N)]
 arr[0] = 1
 arr[1] = 3
-for i in range(2, N):
-    arr[i] = arr[i-2]*2+arr[i-1]
-print(arr[N-1]%10007)
+if N == 1:
+    arr[0] = 1
+else:
+    arr[1] = 3
+    for i in range(2, N):
+        arr[i] = arr[i-2]*2+arr[i-1]
+print(arr[N-1] % 10007)
