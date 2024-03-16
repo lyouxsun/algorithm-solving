6bc822cd8e537f067a7f2df5fbf85c33f6bf1774
이영선 <leedrkr323@konkuk.ac.kr>
2024-03-16 20:54:05 +0900

    Fix BFS - BOJ1987

--- BOJ/graph_traversal/back_tracking/BOJ_1987.py
+++ BOJ/graph_traversal/back_tracking/BOJ_1987.py
@@ -1,6 +1,10 @@
 # 그래프 탐색 - 1987번 - 알파벳
 ## bfs인 줄 알았는데 백트래킹(dfs)이었다,,
-from collections import deque
+# from collections import deque
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 5)
 
 dw = [-1, 1, 0, 0]
 dh = [0, 0, -1, 1]
@@ -9,24 +13,21 @@ alphabet = [list(input()) for _ in range(h)]
 past = set()
 
 
-def bfs():
-    q = deque()
-    q.append([0, 0])
-    past.add(alphabet[0][0])
-    cnt = 1
-
-    while q:
-        now_h, now_w = q.popleft()
-        for i in range(4):
-            nh = now_h + dh[i]
-            nw = now_w + dw[i]
-            if (0 <= nh < h) and (0 <= nw < w):
-                if alphabet[nh][nw] not in past:
-                    past.add(alphabet[nh][nw])
-                    q.append([nh, nw])
-                    cnt += 1
-                    # print('alphabet[', nh, '][', nw, '] = ', alphabet[nh][nw])
-    return cnt
+def dfs(now_h, now_w, cnt):
+    # print(alphabet[now_h][now_w], end=' -> ')
+    global ans
+    ans = max(ans, cnt)
+    for i in range(4):
+        next_h = now_h + dh[i]
+        next_w = now_w + dw[i]
+        if (0 <= next_h < h) and (0 <= next_w < w):
+            if alphabet[next_h][next_w] not in past:
+                past.add(alphabet[next_h][next_w])
+                dfs(next_h, next_w, cnt + 1)
+                past.remove(alphabet[next_h][next_w])
 
 
-print(bfs())
+past.add(alphabet[0][0])
+ans = 0
+dfs(0, 0, 1)
+print(ans)
