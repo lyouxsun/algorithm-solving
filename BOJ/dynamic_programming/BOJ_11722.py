66806260f33f0fd9890103fb92e04820210cfc23
이영선 <leedrkr323@konkuk.ac.kr>
2024-07-22 19:53:16 +0900

    Add DP - BOJ11722

---
+++ BOJ/dynamic_programming/BOJ_11722.py
@@ -0,0 +1,12 @@
+# 동적 프로그래밍 - 11722번 - 가장 긴 감소하는 부분 수열
+
+N = int(input())
+arr = list(map(int, input().split()))
+dp = [1 for _ in range(N)]
+
+for i in range(N):
+    for j in range(0, i):
+        if arr[i] < arr[j] :
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
