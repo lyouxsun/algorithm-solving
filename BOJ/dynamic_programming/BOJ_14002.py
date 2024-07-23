1886c6ee74e0f5ed587276adc104f224e8a6a4ad
이영선 <leedrkr323@konkuk.ac.kr>
2024-07-23 23:03:19 +0900

    Add DP - BOJ14002

--- BOJ/dynamic_programming/BOJ_14002.py
+++ BOJ/dynamic_programming/BOJ_14002.py
@@ -1,27 +1,27 @@
 # 동적 프로그래밍 - 14002번 - 가장 긴 증가하는 부분 수열 4
 ## 이전 문제들과 달리 길이&부분수열 모두를 출력해야 한다.
 
-## 10 20 10 30 20 50
-## 10 20    30
-
-
 N = int(input())
 arr = list(map(int, input().split()))
-dp = [1 for _ in range(N)]
+dp = [1] * N
+prev_index = [-1] * N
 
 for i in range(N):
-    for j in range(0, i):
-        if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j]+1)
+    for j in range(i):
+        if arr[j] < arr[i] and dp[i] < dp[j]+1:
+            dp[i] = dp[j]+1
+            prev_index[i] = j
+# print(dp)
 
-print(dp)
+max_len = max(dp)
+max_idx = dp.index(max_len)
+ans = []
 
-# ans_len = 0
-# ans_index = 0
-# for i in range(N):
-#     if len(dp[i]) > ans_len:
-#         ans_len = len(dp[i])
-#         ans_index = i
-#
-# print(ans_len)
-# print(dp[ans_index])
+while max_idx != -1:
+    ans.append(arr[max_idx])
+    max_idx = prev_index[max_idx]
+
+ans.reverse()
+
+print(max_len)
+print(" ".join(map(str, ans)))
