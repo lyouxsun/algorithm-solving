2803e1b127d4cf8bd6492c5997a61e3a22e816cc
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-09-03 13:22:23 +0900

    Add DataStructure - BOJ9935

---
+++ BOJ/data_structure/stack/BOJ_9935.py
@@ -0,0 +1,25 @@
+# 스택 - 9935번 - 문자열 폭발
+import sys
+input = sys.stdin.readline
+
+arr = list(input().strip())
+n = len(arr)
+bomb = list(input().strip())
+b = len(bomb)
+
+# print(arr)
+# print(bomb)
+
+exp = []
+i = 0
+while i < n:
+    exp.append(arr[i])
+    if len(exp) >= b and exp[-b:] == bomb:
+        del exp[-b:]
+    i += 1
+    # print(exp)
+
+if len(exp) == 0:
+    print('FRULA')
+else:
+    print(''.join(exp))
