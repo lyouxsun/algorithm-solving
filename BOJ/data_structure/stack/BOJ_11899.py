0875ae603ad49e2714d2ad44fdb431f78503c8cf
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-09-06 16:16:10 +0900

    Add DataStructure - BOJ11899

---
+++ BOJ/data_structure/stack/BOJ_11899.py
@@ -0,0 +1,13 @@
+# 스택 - 11899번 - 괄호 끼워넣기
+string = list(input().strip())
+s = []
+ans = 0
+for i in string:
+    if len(s) == 0 or i == '(':
+        s.append(i)
+    elif s[-1] != '(' and i == ')':
+        s.append(i)
+    elif s[-1] == '(' and i == ')':
+        s.pop()
+# print(s)
+print(len(s))
