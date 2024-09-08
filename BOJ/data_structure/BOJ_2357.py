a31622f8bb94881a16feddb86067087efae33fda
lyouxsun <leedrkr323@konkuk.ac.kr>
2024-09-08 21:47:35 +0900

    Fix DataStructure - BOJ2357

--- BOJ/data_structure/BOJ_2357.py
+++ BOJ/data_structure/BOJ_2357.py
@@ -1,54 +1,96 @@
 # 자료구조(세그먼트 트리) - 2357번 - 최솟값과 최댓값
+## n, m -> 구간의 합/차 또는 최댓값/최솟값을 구할 때에는 세그먼트 트리를 사용한다.
 import sys
+
 input = sys.stdin.readline
 
-def init_min_tree(node, start, end):
-    if start == end:
-        seg_min[node] = arr[start]
-    else:
-        mid = (start + end) // 2
-        seg_min[node] = min(init_min_tree(node * 2, start, mid), init_min_tree(node * 2 + 1, mid + 1, end))
-    return seg_min[node]
-
-def init_max_tree(node, start, end):
-    if start == end:
-        seg_max[node] = arr[start]
-    else:
-        mid = (start + end) // 2
-        seg_max[node] = max(init_max_tree(node * 2, start, mid), init_max_tree(node * 2 + 1, mid + 1, end))
-    return seg_max[node]
-
-def query_min(node, start, end, left, right):
-    if left > end or right < start:
-        return float('inf')
-    if left <= start and end <= right:
-        return seg_min[node]
-    mid = (start + end) // 2
-    return min(query_min(node * 2, start, mid, left, right), query_min(node * 2 + 1, mid + 1, end, left, right))
-
-def query_max(node, start, end, left, right):
-    if left > end or right < start:
-        return -float('inf')
-    if left <= start and end <= right:
-        return seg_max[node]
-    mid = (start + end) // 2
-    return max(query_max(node * 2, start, mid, left, right), query_max(node * 2 + 1, mid + 1, end, left, right))
-
 n, m = map(int, input().split())
-arr = []
+arr, ranges = [], []
 for _ in range(n):
     arr.append(int(input()))
-ranges = [list(map(int, input().split())) for _ in range(m)]
+for _ in range(m):
+    ranges.append(list(map(int, input().split())))
 
-size = 2 ** (n - 1).bit_length()
-seg_min = [float('inf')] * (2 * size)
-seg_max = [-float('inf')] * (2 * size)
+# 1. 최댓값/최솟값 세그먼트 초기화
+k = 0
+while 2 ** k < n:  # 2**k >= n 인 k 찾기
+    k += 1
+max_seg, min_seg = [0] * 2 ** k * 2, [0] * 2 ** k * 2
+for i in range(n):
+    max_seg[2 ** k + i] = arr[i]
+    min_seg[2 ** k + i] = arr[i]
+# print(max_seg)
 
-init_min_tree(1, 0, n - 1)
-init_max_tree(1, 0, n - 1)
+## 2-1. 최댓값 세그먼트 트리의 부모노드 채우기
+for idx in range(2 ** k - 1, 0, -1):
+    max_seg[idx] = max(max_seg[2 * idx], max_seg[2 * idx + 1])
+# print('max_seg=', max_seg)
+
+## 2-2. 최솟값 세그먼트 트리의 부모노드 채우기
+for idx in range(2 ** k - 1, 0, -1):
+    if min_seg[2 * idx] == 0 and min_seg[2 * idx + 1] == 0:
+        continue
+    if min_seg[2 * idx] == 0:
+        min_seg[idx] = min_seg[2 * idx + 1]
+    if min_seg[2 * idx + 1] == 0:
+        min_seg[idx] = min_seg[2 * idx]
+    else:
+        min_seg[idx] = min(min_seg[2 * idx], min_seg[2 * idx + 1])
+
+
+# print('min_seg=', min_seg)
+
+# 3. 범위에 따른 최솟값과 최댓값 출력하기
+def segment_max_min(start, end):
+    global k
+    # 인덱스 바꾸기
+    s = start + 2 ** k - 1
+    e = end + 2 ** k - 1
+    # print('[변경된 index] start=%d, end=%d'%(start, end))
+
+    # 최솟값 출력
+    ans = float('inf')
+    while s <= e:
+        if s % 2 == 1:
+            ans = min(ans, min_seg[s])
+            s += 1
+        if e % 2 == 0:
+            ans = min(ans, min_seg[e])
+            e -= 1
+        s //= 2
+        e //= 2
+    if s == e:
+        ans = min(ans, min_seg[s])
+    print(ans, end=' ')
+
+    # 최댓값 출력
+    s = start + 2 ** k - 1
+    e = end + 2 ** k - 1
+    ans = 0
+    while s <= e:
+        # print('ans=',ans)
+        if s % 2 == 1:
+            ans = max(ans, max_seg[s])
+            s += 1
+        if e % 2 == 0:
+            ans = max(ans, max_seg[e])
+            e -= 1
+        s //= 2
+        e //= 2
+    # print('max_seg[start]=%d, max_seg[end]=%d'%(max_seg[s], max_seg[e]))
+    # print('start=%d, end=%d'%(s, e))
+    if s == e:
+        ans = max(ans, max_seg[s])
+    print(ans)
+
 
 for ran in ranges:
-    start, end = ran[0] - 1, ran[1] - 1
-    min_val = query_min(1, 0, n - 1, start, end)
-    max_val = query_max(1, 0, n - 1, start, end)
-    print(min_val, max_val)
+    segment_max_min(ran[0], ran[1])
+
+# 5 1
+# 1
+# 2
+# 3
+# 4
+# 5
+# 1 3
